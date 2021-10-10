491dae2261503db1412fae9b7584b14310ae88bb
Maciej Walkowiak <walkowiak.maciej@yahoo.com>
2021-10-10 20:23:04 +0200

    remove sentry

--- src/main/java/com/example/javacro/JavacroBackendApplication.java
+++ src/main/java/com/example/javacro/JavacroBackendApplication.java
@@ -24,6 +24,7 @@ public class JavacroBackendApplication {
         return args -> {
             movieRepository.save(new Movie("Fight Club", "https://m.media-amazon.com/images/M/MV5BMmEzNTkxYjQtZTc0MC00YTVjLTg5ZTEtZWMwOWVlYzY0NWIwXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg"));
             movieRepository.save(new Movie("Bourne Identity", "https://m.media-amazon.com/images/M/MV5BM2JkNGU0ZGMtZjVjNS00NjgyLWEyOWYtZmRmZGQyN2IxZjA2XkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg"));
+            movieRepository.save(new Movie("Independence Day", "https://m.media-amazon.com/images/I/51+wqGIDRJL._AC_.jpg"));
         };
     }
 }
--- src/main/java/com/example/javacro/MoviesController.java
+++ src/main/java/com/example/javacro/MoviesController.java
@@ -2,20 +2,17 @@ package com.example.javacro;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.Random;
 
 @RestController
 @CrossOrigin
 public class MoviesController {
     private static final Logger LOGGER = LoggerFactory.getLogger(MoviesController.class);
     private final MovieService movieService;
-    private final Random random = new Random();
 
     public MoviesController(MovieService movieService) {
         this.movieService = movieService;
